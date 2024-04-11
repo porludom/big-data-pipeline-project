5e4fc1ab670e979741b6496fac762e6aef379e65
akmchnkv <e.akimchenkova@innopolis.university>
2024-04-11 22:02:12 +0300

    Update create_project.py

--- scripts/create_project.py
+++ scripts/create_project.py
@@ -1,35 +1,44 @@
-import psycopg2 as psql
+"""This module creates a project database and imports data from files."""
+
 from pprint import pprint
 import os
+import psycopg2 as psql
 
 # Read password from secrets file
 file = os.path.join("secrets", ".psql.pass")
-with open(file, "r") as file:
+with open(file, "r", encoding="utf-8") as file:
     password = file.read().rstrip()
 
-# build connection string
-conn_string = "host=hadoop-04.uni.innopolis.ru port=5432 user=team19 dbname=team19_projectdb password={}".format()
+# Build connection string
+connString = (
+    f"host=hadoop-04.uni.innopolis.ru port=5432 user=team19 "
+    f"dbname=team19_projectdb password={password}")
 
 # Connect to the remote dbms
-with psql.connect(conn_string) as conn:
+with psql.connect(connString) as conn:
     # Create a cursor for executing psql commands
     cur = conn.cursor()
     # Read the commands from the file and execute them.
-    with open(os.path.join("sql", "create_db.sql")) as file:
+    with open(os.path.join("sql", "create_db.sql"), encoding="utf-8") as file:
         content = file.read()
         cur.execute(content)
     conn.commit()
 
     # Read the commands from the file and execute them.
-    with open(os.path.join("sql", "import_data.sql")) as file:
+    with open(os.path.join("sql", "import_data.sql"),
+              encoding="utf-8") as file:
         commands = file.readlines()
-        with open(os.path.join("data", "locations.csv"), "r") as locations:
+        with open(os.path.join("data", "locations.csv"), "r",
+                  encoding="utf-8") as locations:
             cur.copy_expert(commands[0], locations)
-        with open(os.path.join("data", "weather_conditions.csv"), "r") as weather_conditions:
+        with open(os.path.join("data", "weather_conditions.csv"), "r",
+                  encoding="utf-8") as weather_conditions:
             cur.copy_expert(commands[1], weather_conditions)
-        with open(os.path.join("data", "astronomical_data.csv"), "r") as astronomical_data:
+        with open(os.path.join("data", "astronomical_data.csv"), "r",
+                  encoding="utf-8") as astronomical_data:
             cur.copy_expert(commands[2], astronomical_data)
-        with open(os.path.join("data", "air_quality.csv"), "r") as air_quality:
+        with open(os.path.join("data", "air_quality.csv"), "r",
+                  encoding="utf-8") as air_quality:
             cur.copy_expert(commands[3], air_quality)
 
     conn.commit()
@@ -37,7 +46,8 @@ with psql.connect(conn_string) as conn:
     pprint(conn)
     cur = conn.cursor()
     # Read the sql commands from the file
-    with open(os.path.join("sql", "test_database.sql")) as file:
+    with open(os.path.join("sql", "test_database.sql"),
+              encoding="utf-8") as file:
         commands = file.readlines()
         for command in commands:
             cur.execute(command)
