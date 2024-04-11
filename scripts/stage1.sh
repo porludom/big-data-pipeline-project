1a225db12a0a2ea358a290d48cad08a637fcea47
Roman Voronov <47979693+porludom@users.noreply.github.com>
2024-04-11 15:01:22 +0300

    Update stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,3 +1,3 @@
 python ~/project/big-data-pipeline-project/scripts/build_projectdb.py
 hdfs dfs -rm -r /user/team19/project/warehouse/*
-sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team19_projectdb --username team19 --password PxDhQyxhalNM21mA --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team19_projectdb --username team19 --password $(<~/password) --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
