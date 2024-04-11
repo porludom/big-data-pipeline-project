0b74c12c5df174d4878ad0f8b357ca98f29b1fce
roman <team19@hadoop-01.uni.innopolis.ru>
2024-04-11 14:58:41 +0300

    123

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,3 @@
 python scripts/build_projectdb.py
+hdfs dfs -rm -r /user/team19/project/warehouse/*
+sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team19_projectdb --username team19 --password PxDhQyxhalNM21mA --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
