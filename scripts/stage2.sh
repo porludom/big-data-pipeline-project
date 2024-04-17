8061f332912cd2f7336937335eb834d1e8f19fa2
Viktoria <88908152+mpvnlv@users.noreply.github.com>
2024-04-17 16:05:53 +0300

    Update stage2.sh

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -3,4 +3,4 @@ hdfs dfs -put output/*.avsc project/warehouse/avsc
 Password=$(<~/password) 
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/db.hql
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/db.hql > output/hive_results.txt 2> /dev/null
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/optimizations.hql > /dev/null 2> /dev/null
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/optimizations.hql 
