617990784b72455dc405f4b9c0b33b18432f22af
Viktoria <88908152+mpvnlv@users.noreply.github.com>
2024-05-07 22:41:11 +0300

    Update stage4.sh

--- scripts/stage4.sh
+++ scripts/stage4.sh
@@ -1,2 +1,2 @@
 Password=$(<~/password) 
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/stage4.hql
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team19 -p $Password -f sql/stage4.hql > output/evaluation.txt
