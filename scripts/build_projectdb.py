3f26e4ecb02cae5e36b92f08b2321e65025506c6
roman <team19@hadoop-01.uni.innopolis.ru>
2024-05-07 20:05:19 +0300

    123

--- scripts/build_projectdb.py
+++ scripts/build_projectdb.py
@@ -18,25 +18,31 @@ with psql.connect(connString) as conn:
     # Create a cursor for executing psql commands
     cur = conn.cursor()
     # Read the commands from the file and execute them.
-    with open(os.path.join("../project/big-data-pipeline-project/sql", "create_db.sql"), encoding="utf-8") as file:
+    with open(os.path.join("../project/big-data-pipeline-project/sql",
+                           "create_db.sql"), encoding="utf-8") as file:
         content = file.read()
         cur.execute(content)
     conn.commit()
 
     # Read the commands from the file and execute them.
-    with open(os.path.join("../project/big-data-pipeline-project/sql", "import_data.sql"),
+    with open(os.path.join("../project/big-data-pipeline-project/sql",
+                           "import_data.sql"),
               encoding="utf-8") as file:
         commands = file.readlines()
-        with open(os.path.join("../project/big-data-pipeline-project/data", "locations.csv"), "r",
+        with open(os.path.join("../project/big-data-pipeline-project/data",
+                               "locations.csv"), "r",
                   encoding="utf-8") as locations:
             cur.copy_expert(commands[0], locations)
-        with open(os.path.join("../project/big-data-pipeline-project/data", "weather_conditions.csv"), "r",
+        with open(os.path.join("../project/big-data-pipeline-project/data",
+                               "weather_conditions.csv"), "r",
                   encoding="utf-8") as weather_conditions:
             cur.copy_expert(commands[1], weather_conditions)
-        with open(os.path.join("../project/big-data-pipeline-project/data", "astronomical_data.csv"), "r",
+        with open(os.path.join("../project/big-data-pipeline-project/data",
+                               "astronomical_data.csv"), "r",
                   encoding="utf-8") as astronomical_data:
             cur.copy_expert(commands[2], astronomical_data)
-        with open(os.path.join("../project/big-data-pipeline-project/data", "air_quality.csv"), "r",
+        with open(os.path.join("../project/big-data-pipeline-project/data",
+                               "air_quality.csv"), "r",
                   encoding="utf-8") as air_quality:
             cur.copy_expert(commands[3], air_quality)
 
--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,1 +1,1 @@
-
+python3 ml.py
